c4dacc80a1179b3f89ab269e2805731de7ea6346
Ali Haghighatkhah <ali@lightcurve.io>
2023-05-14 21:46:52 +0200

    Add subscriptions endpoint to gateway WIP

---
+++ services/gateway/apis/http-version3/methods/subscriptions.js
@@ -0,0 +1,28 @@
+/*
+ * LiskHQ/lisk-service
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+const subscriptionsSource = require('../../../sources/version3/subscriptions');
+const envelope = require('../../../sources/version3/mappings/stdEnvelope');
+const regex = require('../../../shared/regex');
+const { transformParams, response, getSwaggerDescription } = require('../../../shared/utils');
+
+module.exports = {
+	version: '2.0',
+	swaggerApiPath: '/subscriptions',
+  tags: ['Subscriptions'],
+  source: subscriptionsSource,
+	envelope: {},
+};
