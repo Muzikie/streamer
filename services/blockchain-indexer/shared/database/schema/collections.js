5060b753e4cd3c7d3008317b3201de4a0911bdfe
Ali Haghighatkhah <ali@lightcurve.io>
2023-05-31 14:17:54 +0200

    Fix databsae schema mispatch

--- services/blockchain-indexer/shared/database/schema/collections.js
+++ services/blockchain-indexer/shared/database/schema/collections.js
@@ -5,10 +5,9 @@ module.exports = {
 		collectionID: { type: 'string', null: true, defaultValue: null },
 		name: { type: 'string', null: true, defaultValue: null },
 		releaseYear: { type: 'string', null: true, defaultValue: null },
-		collectionType: { type: 'uint32', null: true, defaultValue: null },
-		audios: { type: 'array', null: true, defaultValue: null },
-		coverSignature: { type: 'bytes', null: true, defaultValue: null },
-		coverHash: { type: 'bytes', null: true, defaultValue: null },
+		collectionType: { type: 'integer', null: true, defaultValue: null },
+		coverSignature: { type: 'string', null: true, defaultValue: null },
+		coverHash: { type: 'string', null: true, defaultValue: null },
 		creatorAddress: { type: 'string', null: true, defaultValue: null },
 	},
 	indexes: {
