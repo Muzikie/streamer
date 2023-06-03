f04baba072359ce9f6487864f02225688e5f3356
curvesy <noobesang@gmail.com>
2023-06-03 05:04:51 +0330

    Added updatedCollection function to applyTransactoin in SetAttribute for collection

--- services/blockchain-indexer/shared/indexer/transactionProcessor/collection/attributeSet.js
+++ services/blockchain-indexer/shared/indexer/transactionProcessor/collection/attributeSet.js
@@ -3,25 +3,16 @@ const {
 	MySQL: { getTableInstance },
 } = require('lisk-service-framework');
 
-const { getLisk32AddressFromPublicKey } = require('../../../utils/account');
-
 const config = require('../../../../config');
 
 const logger = Logger();
 
 const MYSQL_ENDPOINT = config.endpoints.mysql;
-const accountsTableSchema = require('../../../database/schema/accounts');
 const collectionsTableSchema = require('../../../database/schema/collections');
 const {
 	MODULE_NAME_COLLECTION,
 	EVENT_NAME_COLLECTION_ATTRIBUTESET,
 } = require('../../../../../blockchain-connector/shared/sdk/constants/names');
-
-const getAccountsTable = () => getTableInstance(
-	accountsTableSchema.tableName,
-	accountsTableSchema,
-	MYSQL_ENDPOINT,
-);
 
 const getCollectionsTable = () => getTableInstance(
 	collectionsTableSchema.tableName,
@@ -30,39 +21,32 @@ const getCollectionsTable = () => getTableInstance(
 );
 
 // Command specific constants
-const COMMAND_NAME = 'attributeSet';
+const COMMAND_NAME = 'setAttributes';
+
+const updateCollection = async (collectionsTable, collectionID, updates, dbTrx) => {
+	const where = {
+		collectionID,
+	};
+
+	await collectionsTable.update({ where, updates }, dbTrx);
+	logger.debug(`Updated collection with ID ${collectionID}.`);
+};
 
 // eslint-disable-next-line no-unused-vars
 const applyTransaction = async (blockHeader, tx, events, dbTrx) => {
-	// const accountsTable = await getAccountsTable();
 	const collectionsTable = await getCollectionsTable();
-
-	// const senderAddress = getLisk32AddressFromPublicKey(tx.senderPublicKey);
-
-	// const account = {
-	// 	address: senderAddress,
-	// };
-
-	// 	logger.trace(`Updating account index for the account with address ${account.address}.`);
-	// 	await accountsTable.upsert(account, dbTrx);
-	// 	logger.debug(`Updated account index for the account with address ${account.address}.`);
-	// 	logger.trace(`Indexing collections with address ${account.address}.`);
 
 	const { data: eventData = {} } = events.find(
 		({ module, name }) => module === MODULE_NAME_COLLECTION
 			&& name === EVENT_NAME_COLLECTION_ATTRIBUTESET,
 	);
 
-	// Thanks to blockchain logic, collectionNFTs will remain in hands of it's actual owner 
-	// and there are no possible vulnerability to change the owner of a CollectionNFT via fraud
-	// then we don't need to implement the logic here for preventing creatorAddress change
-	const collectionsNFT = {
+	const { collectionID, ...updates } = {
 		...eventData,
 		...tx.params,
 	};
 
-	await collectionsTable.update(collectionsNFT, dbTrx);
-	logger.debug(`Indexed collection with ID ${eventData.collectionsID}.`);
+	await updateCollection(collectionsTable, collectionID, updates, dbTrx);
 };
 
 // eslint-disable-next-line no-unused-vars
