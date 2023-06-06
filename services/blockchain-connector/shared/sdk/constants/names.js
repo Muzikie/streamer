3562ba26cee905ef383043b3cd2ddf3b1b85a816
Ali Haghighatkhah <ali@muzikie.com>
2023-06-06 22:49:51 +0200

    Add support for indexing audio:streamed transactions

--- services/blockchain-connector/shared/sdk/constants/names.js
+++ services/blockchain-connector/shared/sdk/constants/names.js
@@ -97,6 +97,7 @@ const EVENT_NAME_COLLECTION_CREATED = 'collectionCreated';
 // Audios
 const MODULE_NAME_AUDIO = 'audio';
 const EVENT_NAME_AUDIO_CREATED = 'audioCreated';
+const EVENT_NAME_AUDIO_STREAMED = 'audioStreamed';
 
 module.exports = {
 	MODULE_NAME_AUTH,
@@ -169,4 +170,5 @@ module.exports = {
 
 	MODULE_NAME_AUDIO,
 	EVENT_NAME_AUDIO_CREATED,
+	EVENT_NAME_AUDIO_STREAMED,
 };
